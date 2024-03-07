39d6ab22a90f5c3fde394e568684acc4015e1727
nobody <nobodyisherefornow1977@gamil.com>
2024-03-07 03:08:55 -0500

    commit after test(2) updates[ time out = 120s pending-> stop uncompleted tasks asyncio.wait() -> to make sure stop after receive the code's 202 webbrowser.open()-> open the url in web ]

--- digimovie_founder.py
+++ digimovie_founder.py
@@ -1,37 +1,39 @@
 import webbrowser
-
 import aiohttp
 import asyncio
 
 
-async def found_one(url) -> dict:
+# import signal
+# import logging
+
+
+async def found_one(url: str) -> None:
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as answer:
                 print(f'success url : {url}')
-                return {answer.status: url}
+                webbrowser.open_new(url)
     except aiohttp.client_exceptions.ClientConnectorError:
-        print(f'field url : {url}')
-        return {404: url}
+        await asyncio.sleep(180)
 
 
-async def found_all():  # found the correct link of now that's up
+async def found_all() -> set:  # found the correct link of now which is up
+    def stop(stuff):
+        print(f'{len(stuff)} link checked . . .')
+        for task in stuff:
+            task.cancel()
+
     print('try to find the current link . . .')
-    # site_name = "https://digimovie54.top"
-    task = [found_one(f"https://digimovie{i}.top") for i in range(1, 100)]
-    status = await asyncio.gather(*task)
-    return status
+    # "https://digimovie54.top"
+    tasks = [asyncio.create_task(found_one(f"https://digimovie{i}.top")) for i in range(1, 100)]
+    done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED, timeout=120)
+    print(done)
+    stop(pending)
+    return done
 
 
-async def main():  # mian function to run
-    status_dic = await found_all()
-    for i in status_dic:
-        print(i)
-    # url = status_dic[200]
-    # print(f"current url is : {url}")
-    # webbrowser.open_new_tab(url)
-    loop.close()
-
+async def main() -> None:  # mian function to run
+    await found_all()
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
@@ -39,4 +41,6 @@ if __name__ == '__main__':
         loop.run_until_complete(main())
     except KeyboardInterrupt:
         print('interrupted')
+    finally:
+        loop.stop()
         loop.close()
