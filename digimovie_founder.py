b5b729da6a64ae1f5c6130febe58c6eee8ba8096
nobody <nobodyisherefornow1977@gamil.com>
2024-03-09 03:17:52 -0500

    handel some exceptions and add sigal for shutdown

--- digimovie_founder.py
+++ digimovie_founder.py
@@ -1,4 +1,6 @@
+import signal
 import webbrowser
+
 import aiohttp
 import asyncio
 
@@ -17,30 +19,39 @@ async def found_one(url: str) -> None:
         await asyncio.sleep(180)
 
 
-async def found_all() -> set:  # found the correct link of now which is up
-    def stop(stuff):
-        print(f'{len(stuff)} link checked . . .')
-        for task in stuff:
-            task.cancel()
+def shutdown():
+    tasks0 = []
+    for task in asyncio.all_tasks(loop):
+        task.cancel()
+        tasks0.append(task)
+    result = asyncio.gather(*tasks0)
+    print(f'result: {result}')
+    loop.stop()
 
+
+async def found_all() -> set:  # found the correct link of now which is up
     print('try to find the current link . . .')
     # "https://digimovie54.top"
     tasks = [asyncio.create_task(found_one(f"https://digimovie{i}.top")) for i in range(1, 100)]
     done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED, timeout=120)
     print(done)
-    stop(pending)
+    shutdown()
     return done
 
 
 async def main() -> None:  # mian function to run
     await found_all()
 
+
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGINT, shutdown)
     try:
         loop.run_until_complete(main())
     except KeyboardInterrupt:
         print('interrupted')
-    finally:
-        loop.stop()
-        loop.close()
+        shutdown()
+    except asyncio.exceptions.CancelledError:
+        print('cancelled')
+
+
